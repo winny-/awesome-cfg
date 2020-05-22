51eeca944dca1cd4e387b53f93180dca5ce59fd9
Winston Weinert <winston@ml1.net>
2020-05-21 20:50:03 -0500

    break out signals

--- rc.lua
+++ rc.lua
@@ -12,7 +12,7 @@ local defaults = require './defaults'
 local keys = require './keys'
 local buttons = require './buttons'
 
-
+local signals = require './signals'
 
 require './error_handling'
 
@@ -32,59 +32,4 @@ root.buttons(buttons.global)
 require './bar'
 
 
--- Signal function to execute when a new client appears.
-client.connect_signal("manage", function (c)
-                         -- Set the windows at the slave,
-                         -- i.e. put it at the end of others instead of setting it master.
-                         -- if not awesome.startup then awful.client.setslave(c) end
-
-                         if awesome.startup and
-                            not c.size_hints.user_position
-                         and not c.size_hints.program_position then
-                            -- Prevent clients from being unreachable after screen count changes.
-                            awful.placement.no_offscreen(c)
-                         end
-end)
-
--- Add a titlebar if titlebars_enabled is set to true in the rules.
-client.connect_signal("request::titlebars", function(c)
-                         -- buttons for the titlebar
-                         local buttons = buttons.make_titlebar(c)
-
-                         awful.titlebar(c) : setup {
-                            { -- Left
-                               awful.titlebar.widget.iconwidget(c),
-                               buttons = buttons,
-                               layout  = wibox.layout.fixed.horizontal
-                            },
-                            { -- Middle
-                               { -- Title
-                                  align  = "center",
-                                  widget = awful.titlebar.widget.titlewidget(c)
-                               },
-                               buttons = buttons,
-                               layout  = wibox.layout.flex.horizontal
-                            },
-                            { -- Right
-                               awful.titlebar.widget.floatingbutton (c),
-                               awful.titlebar.widget.maximizedbutton(c),
-                               awful.titlebar.widget.stickybutton   (c),
-                               awful.titlebar.widget.ontopbutton    (c),
-                               awful.titlebar.widget.closebutton    (c),
-                               layout = wibox.layout.fixed.horizontal()
-                            },
-                            layout = wibox.layout.align.horizontal
-                                                   }
-end)
-
--- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal("mouse::enter", function(c)
-                         if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
-                         and awful.client.focus.filter(c) then
-                            client.focus = c
-                         end
-end)
-
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
-
+signals.connect()
---
+++ signals.lua
@@ -0,0 +1,79 @@
+local gears = require 'gears'
+local awful = require 'awful'
+local beautiful = require 'beautiful'
+local wibox = require 'wibox'
+
+local buttons = require './buttons'
+
+local callbacks = {
+    manage_cb = function (c)
+        -- Set the windows at the slave,
+        -- i.e. put it at the end of others instead of setting it master.
+        -- if not awesome.startup then awful.client.setslave(c) end
+
+        if awesome.startup and
+            not c.size_hints.user_position
+        and not c.size_hints.program_position then
+            -- Prevent clients from being unreachable after screen count changes.
+            awful.placement.no_offscreen(c)
+        end
+    end,
+    request_titlebars_cb = function(c)
+        -- buttons for the titlebar
+        local buttons = buttons.make_titlebar(c)
+
+        awful.titlebar(c) : setup {
+            { -- Left
+                awful.titlebar.widget.iconwidget(c),
+                buttons = buttons,
+                layout  = wibox.layout.fixed.horizontal
+            },
+            { -- Middle
+                { -- Title
+                    align  = "center",
+                    widget = awful.titlebar.widget.titlewidget(c)
+                },
+                buttons = buttons,
+                layout  = wibox.layout.flex.horizontal
+            },
+            { -- Right
+                awful.titlebar.widget.floatingbutton (c),
+                awful.titlebar.widget.maximizedbutton(c),
+                awful.titlebar.widget.stickybutton   (c),
+                awful.titlebar.widget.ontopbutton    (c),
+                awful.titlebar.widget.closebutton    (c),
+                layout = wibox.layout.fixed.horizontal()
+            },
+            layout = wibox.layout.align.horizontal
+                                  }
+    end,
+    mouse_enter_cb = function(c)
+        if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
+        and awful.client.focus.filter(c) then
+            client.focus = c
+        end
+    end,
+    focus_cb = function(c)
+        c.border_color = beautiful.border_focus
+    end,
+    unfocus_cb = function(c)
+        c.border_color = beautiful.border_normal
+    end
+}
+
+local function connect()
+    -- Signal function to execute when a new client appears.
+    client.connect_signal('manage', callbacks.manage_cb)
+
+    -- Add a titlebar if titlebars_enabled is set to true in the rules.
+    client.connect_signal("request::titlebars", callbacks.request_titlebars_cb)
+
+    -- Enable sloppy focus, so that focus follows mouse.
+    client.connect_signal("mouse::enter", callbacks.mouse_enter_cb)
+
+    -- Indicate focused frame
+    client.connect_signal("focus", callbacks.focus_cb)
+    client.connect_signal("unfocus", callbacks.unfocus_cb)
+end
+
+return gears.table.join(callbacks, {connect = connect})
