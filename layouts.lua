e1920f0332e2268799f1b3aa6963cbf896ec571a
Winston Weinert <winston@ml1.net>
2020-06-04 04:51:48 -0500

    Add dynamite & implement a stack layout

--- layouts.lua
+++ layouts.lua
@@ -1,6 +1,25 @@
 local suit = require 'awful.layout.suit'
 local lain = require 'lain.layout'
 
+local dynamite = require 'dynamite'
+
+local stack = dynamite {
+    name = 'stack',
+    {
+        reflow       = true,
+        max_elements = 1,
+        priority     = 2,
+        ratio        = 0.20,
+        layout       = dynamite.layout.ratio.vertical
+    },
+    {
+        priority = 1,
+        layout    = dynamite.layout.stack
+    },
+    inner_fill_strategy = 'default',
+    layout     = dynamite.layout.ratio.horizontal
+}
+
 -- Table of layouts to cover with awful.layout.inc, order matters.
 return {
    suit.floating,
@@ -17,4 +36,5 @@ return {
    lain.centerwork,
    lain.cascade,
    lain.termfair,
+   stack,
 }
