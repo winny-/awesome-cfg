e30cf8628bedb58679c3039e1fa1221d407383ef
Winston Weinert <winston@ml1.net>
2020-06-20 22:55:50 -0500

    disable dynamite stuff temporarily

--- layouts.lua
+++ layouts.lua
@@ -1,38 +1,38 @@
 local suit = require 'awful.layout.suit'
 local lain = require 'lain.layout'
 
-local dynamite = require 'dynamite'
+-- local dynamite = require 'dynamite'
 
-local stack = dynamite {
-    name = 'stack',
-    {
-        reflow       = true,
-        max_elements = 1,
-        priority     = 2,
-        ratio        = 0.20,
-        layout       = dynamite.layout.ratio.vertical
-    },
-    {
-        priority = 1,
-        layout    = dynamite.layout.stack
-    },
-    inner_fill_strategy = 'default',
-    layout     = dynamite.layout.ratio.horizontal
-}
+-- local stack = dynamite {
+--   name = 'stack',
+--   {
+--     reflow       = true,
+--     max_elements = 1,
+--     priority     = 2,
+--     ratio        = 0.20,
+--     layout       = dynamite.layout.ratio.vertical
+--   },
+--   {
+--     priority = 1,
+--     layout    = dynamite.layout.stack
+--   },
+--   inner_fill_strategy = 'default',
+--   layout     = dynamite.layout.ratio.horizontal
+-- }
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 return {
-   suit.floating,
-   suit.tile,
-   suit.tile.bottom,
-   suit.fair,
-   suit.fair.horizontal,
-   suit.spiral.dwindle,
-   suit.max,
-   suit.magnifier,
-   suit.corner.nw,
-   lain.centerwork,
-   lain.cascade,
-   lain.termfair,
-   stack,
+  suit.floating,
+  suit.tile,
+  suit.tile.bottom,
+  suit.fair,
+  suit.fair.horizontal,
+  suit.spiral.dwindle,
+  suit.max,
+  suit.magnifier,
+  suit.corner.nw,
+  lain.centerwork,
+  lain.cascade,
+  lain.termfair,
+--  stack,
 }
