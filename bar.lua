6f1c785419e90424f23a7f021b2a4eaeceb5376e
Winston Weinert <winston@ml1.net>
2021-04-17 14:13:53 -0500

    Add small separator bars between tasklist entries

--- bar.lua
+++ bar.lua
@@ -144,7 +144,22 @@ return {
         s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, taglist_buttons)
 
         -- Create a tasklist widget
-        s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklist_buttons, {tasklist_disable_icon=false})
+        s.mytasklist = awful.widget.tasklist {
+            screen = s,
+            filter = awful.widget.tasklist.filter.currenttags,
+            buttons = tasklist_buttons,
+            style = {
+                tasklist_disable_icon = false
+            },
+            layout = {
+                spacing_widget = {
+                    color = '#555',
+                    widget = wibox.widget.separator
+                },
+                spacing = 1,
+                layout = wibox.layout.flex.horizontal
+            }
+        }
 
         -- Create the wibox
         s.mywibox = awful.wibar({ position = "top", screen = s })
